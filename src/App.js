e58d186ff85f7b2214fe7ec6f4bcaed250339f47
Harish1611 <harishnaidub005@gmail.com>
2023-10-25 14:19:49 +0530

    Extended: Added useEffect hook for localStorage Verification to avoid re-render

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
@@ -7,10 +7,13 @@ import MainHeader from "./components/MainHeader/MainHeader";
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const localStoredStatus = localStorage.getItem("isLoggedIn");
-  if (localStoredStatus === "1") {
-    setIsLoggedIn(true);
-  }
+  useEffect(() => {
+    const localStoredStatus = localStorage.getItem("isLoggedIn");
+    if (localStoredStatus === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
     localStorage.setItem("isLoggedIn", "1");
 
