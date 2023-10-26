2aa279d942d0035ab7e5b9a76904976a5217ee33
Harish1611 <harishnaidub005@gmail.com>
2023-10-27 03:56:19 +0530

    Modified: added forwardref to understand and explore it along with useImperativeHandle

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, useReducer, useContext } from "react";
+import React, {
+  useState,
+  useEffect,
+  useReducer,
+  useContext,
+  useRef,
+} from "react";
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
@@ -75,6 +81,10 @@ const Login = (props) => {
   }, [emailIsValid, collegeIsValid, passwordIsValid]);
 
   const authCtx = useContext(AuthContext);
+  const emailInputRef = useRef();
+  const collegeInputRef = useRef();
+  const passwordInputRef = useRef();
+
   const emailChangeHandler = (event) => {
     dispatchEmail({ type: "USER_INPUT", val: event.target.value });
 
@@ -118,13 +128,22 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    authCtx.onLogin(emailState.value, collgeState.value, passwordState.value);
+    if (formIsValid) {
+      authCtx.onLogin(emailState.value, collgeState.value, passwordState.value);
+    } else if (!emailIsValid) {
+      emailInputRef.current.focus();
+    } else if (!collegeIsValid) {
+      collegeInputRef.current.focus();
+    } else {
+      passwordInputRef.current.focus();
+    }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <Input
+          ref={emailInputRef}
           id="email"
           label="E-Mail"
           type="email"
@@ -135,6 +154,7 @@ const Login = (props) => {
         />
 
         <Input
+          ref={collegeInputRef}
           id="college"
           label="College"
           type="text"
@@ -145,6 +165,7 @@ const Login = (props) => {
         />
 
         <Input
+          ref={passwordInputRef}
           id="password"
           label="Password"
           type="password"
@@ -154,7 +175,7 @@ const Login = (props) => {
           onBlur={validatePasswordHandler}
         />
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn}>
             Login
           </Button>
         </div>
